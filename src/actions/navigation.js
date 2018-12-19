cd92c6db4e7eea43078c7328009b5a9a3ee53618
Simon <simon@earnwithdrop.com>
2018-12-18 23:02:35 -0500

    updates open and close loading indicator action creators to have default argument values

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -1,9 +1,18 @@
-export const openLoadingIndicator = ({ text, onOpen }) => ({
+import noop from 'lodash/noop'
+
+export const openLoadingIndicator = ({
+  text = 'loading',
+  onOpen = noop,
+} = {}) => ({
   type: 'OPEN_LOADING_INDICATOR',
   payload: { text, onOpen },
 })
 
-export const closeLoadingIndicator = ({ success, text, onClose }) => ({
+export const closeLoadingIndicator = ({
+  success = true,
+  text = 'success!',
+  onClose = noop,
+} = {}) => ({
   type: 'CLOSE_LOADING_INDICATOR',
   payload: { success, text, onClose },
 })
