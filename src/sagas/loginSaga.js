86c572796b09e70de7423ca6853332d1ffb9ee5f
Simon <simon@earnwithdrop.com>
2018-12-18 23:03:48 -0500

    adds loading indicators to login saga and signup saga

--- src/sagas/loginSaga.js
+++ src/sagas/loginSaga.js
@@ -2,13 +2,25 @@ import { take, takeLatest, put, select } from 'redux-saga/effects'
 import get from 'lodash/get'
 import { fetchCurrentUser } from '../actions/user'
 import { getCurrentUser } from '../selectors/user'
+import { getLoadingIndicator } from '../selectors/navigation'
+import {
+  openLoadingIndicator,
+  closeLoadingIndicator,
+} from '../actions/navigation'
 
 function* loginSaga(action) {
+  const indicator = yield select(getLoadingIndicator)
+
+  if (!indicator.isOpen) {
+    yield put(openLoadingIndicator())
+  }
+
   const loginResponseAction = yield take(action =>
     /^USER_LOGIN_REQUEST_/.test(action.type)
   )
 
   if (/FAILURE$/.test(loginResponseAction.type)) {
+    yield put(closeLoadingIndicator({ success: 'false', text: 'login failed' }))
     return
   }
 
@@ -23,10 +35,11 @@ function* loginSaga(action) {
   )
 
   if (/FAILURE$/.test(currentUserResponseAction.type)) {
+    yield put(closeLoadingIndicator({ success: 'false', text: 'login failed' }))
     return
   }
 
-  const currentUser = yield select(getCurrentUser)
+  yield put(closeLoadingIndicator())
 }
 
 export default function* watchLoginSaga() {
--- src/sagas/signupSaga.js
+++ src/sagas/signupSaga.js
@@ -1,17 +1,26 @@
 import { take, takeLatest, put, call, select } from 'redux-saga/effects'
 import { userLoginRequest } from '../actions/user'
+import {
+  openLoadingIndicator,
+  closeLoadingIndicator,
+} from '../actions/navigation'
 
 function* signupSaga(action) {
+  yield put(openLoadingIndicator())
+
   const signupResponseAction = yield take(action =>
     /^USER_SIGNUP_REQUEST_/.test(action.type)
   )
 
   if (/FAILURE$/.test(signupResponseAction.type)) {
+    yield put(closeLoadingIndicator({ success: false, text: 'signup failed' }))
     return
   }
 
   const {
-    payload: { email, password },
+    payload: {
+      body: { email, password },
+    },
   } = action
 
   yield put(userLoginRequest({ email, password }))
