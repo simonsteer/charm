a4d882f83f1fe6e3a85c3f8d0a6068ce3906fc86
Simon <simon@earnwithdrop.com>
2018-12-24 17:41:21 -0500

    updates gitignore, adds headercomponent to list of user messages

--- src/screens/UserMessagesScreen/UserMessagesScreen.js
+++ src/screens/UserMessagesScreen/UserMessagesScreen.js
@@ -3,6 +3,7 @@ import { FlatList, StyleSheet, KeyboardAvoidingView } from 'react-native'
 import Screen from '../../interface/Screen'
 import UserMessagesHeader from './UserMessagesHeader'
 import Flex from '../../interface/Flex'
+import Text from '../../interface/Text'
 import Button from '../../interface/Button'
 import TextInput from '../../interface/TextInput'
 import MessageSlat from './MessageSlat'
@@ -32,6 +33,7 @@ export default class UserMessagesScreen extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      hasReachedFirstMessage: false,
       messages: MOCK_MESSAGES,
       messageText: '',
     }
@@ -49,6 +51,7 @@ export default class UserMessagesScreen extends Component {
           data={messages}
           keyExtractor={(item, index) => `message-${index}`}
           renderItem={this.renderMessages}
+          ListHeaderComponent={this.renderHeaderComponent}
         />
         <KeyboardAvoidingView
           behavior="padding"
@@ -88,6 +91,18 @@ export default class UserMessagesScreen extends Component {
 
   renderMessages = ({ item: message, index }) => (
     <MessageSlat {...message} index={index} />
+  )
+
+  renderHeaderComponent = () => (
+    <Text
+      center
+      color="darkGrey"
+      lineHeight={18}
+      size="small"
+      style={{ marginVertical: 16, paddingHorizontal: 32 }}
+    >
+      This is the beginning of your message history with DisplayName.
+    </Text>
   )
 }
 
