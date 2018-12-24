11174c8b0235be3d9293682d7ca13010b43b16b6
Simon <simon@earnwithdrop.com>
2018-12-23 19:12:44 -0500

    updates AlertRouter to use same export pattern as other components put into their own folders

--- src/navigation/AlertRouter/index.js
+++ src/navigation/AlertRouter/index.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import { ALERTS } from './constants'
-
-const AlertRouter = ({ alert, closeAlert }) => {
-  const AlertComponent = ALERTS[alert.name]
-
-  if (!AlertComponent) {
-    return null
-  }
-
-  return <AlertComponent params={alert.params} closeAlert={closeAlert} />
-}
-
-export default AlertRouter
+export default from './AlertRouter'
